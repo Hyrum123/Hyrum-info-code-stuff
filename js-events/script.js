d52f835e92e33b79c64e986da7d2bdb9487138ca
Hyrum Whetten <jeromehyhy@gmail.com>
2026-04-17 17:25:22 +0000

    niceeeeeee

---
+++ js-events/script.js
@@ -0,0 +1,25 @@
+// function greet(name){
+//     //console.log(`Greetings fellow ${name}`);
+//     console.log("Hello " + name)
+// }
+
+// greet("yo mama");
+
+// function addNumbers(num1, num2) {
+//     let sum = num1 + num2;
+//     alert("Sum is equal to " + sum);
+// }
+
+// //addNumbers(6, 7);
+
+function articles1() {
+    alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.")
+}
+
+function articles2() {
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.")
+}
+
+function articles3() {
+    alert ("We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.")
+}
