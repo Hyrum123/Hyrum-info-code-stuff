01f7b522425e120905134c1c6534450705c7c274
Hyrum Whetten <jeromehyhy@gmail.com>
2026-04-20 17:21:12 +0000

    ahi ta profesorski

---
+++ js-variables/script.js
@@ -0,0 +1,25 @@
+function function1() {
+    const name = "Hyrum";
+    const color = "Green";
+    let loc = "School";
+    let mood = "Super Good";
+    console.log(`${name}, ${color}, ${loc}, ${mood}`);
+}
+
+function function2() {
+    const companyName = "Bentley Products";
+    const foundingYear = 2026;
+    let isActive = true;
+    let fundingAmount;
+    console.log(`${companyName}, ${foundingYear}, ${isActive}, ${fundingAmount}`);
+}
+
+function function3() {
+    let num = 10;
+    let x = num;
+    x = x * 2;
+    x = x + 10;
+    x = x / 2;
+    x = x - num;
+    alert("The value is " + x);
+}
