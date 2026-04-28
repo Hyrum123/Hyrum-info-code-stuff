c4a3a949581bc657d497e3c3f278c0add0baae7e
Hyrum Whetten <jeromehyhy@gmail.com>
2026-04-28 16:39:53 +0000

    im a pro man

--- js-coin/script.js
+++ js-coin/script.js
@@ -3,10 +3,12 @@ function getRandomInt(max) {
 }
 
 function flip() {
-    let coin = document.querySelector('button');
+    let coin = document.querySelector('img');
     if (getRandomInt(2) == 1) {
-        coin.innerHTML = "Heads";
+        coin.src = "https://preservegold.com/wp-content/uploads/2025/03/Morgan-Silver-Dollar-front.webp"
+        console.log("Heads");
     } else {
-        coin.innerHTML = "Tails";
+        coin.src = "https://m.media-amazon.com/images/I/81KVepxfQhL._AC_UF894,1000_QL80_.jpg"
+        console.log("Tails");
     }
 }
