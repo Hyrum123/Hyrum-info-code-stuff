f693f82c7cbc0706c41cb962ac6fdbb8b8325f58
Hyrum Whetten <jeromehyhy@gmail.com>
2026-05-12 16:39:01 +0000

    ahi ta profe

--- intervals/script.js
+++ intervals/script.js
@@ -1,19 +1,44 @@
-let counter = 10;
-const timer = new Audio("./alarm.mp3");
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#one').onclick = function(){
+        counter = 60;
+        document.querySelector('h1').innerHTML = "1:00";
+    }
+        document.querySelector('#five').onclick = function(){
+        counter = 300;
+        document.querySelector('h1').innerHTML = "5:00";
+    }
+        document.querySelector('#ten').onclick = function(){
+        counter = 600;
+        document.querySelector('h1').innerHTML = "10:00";
+    }
+});
+
+let counter = 67;
+const timer = new Audio("./thriller.mp3");
 
 function count() {
     if(counter > 0) {
         counter--;
-    } else if(counter == 0) {
+        let mins = Math.floor(counter/60);
+        let seconds = counter-(mins*60);
+        if (seconds < 10) {
+            seconds = "0"+seconds;
+        }
+        document.querySelector('h1').innerHTML = mins + ":" + seconds;
+    }
+    if(counter == 0) {
         timer.currentTime = 0;
         timer.play();
+        counter--;
     }
-    document.querySelector('h1').innerHTML = counter;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = function(){
+    document.querySelector('#start').onclick = function(){
         setInterval(count, 1000);
-        document.querySelector("button").disabled = true;
+        document.querySelector("#start").disabled = true;
+        document.querySelector("#one").disabled = true;
+        document.querySelector("#five").disabled = true;
+        document.querySelector("#ten").disabled = true;
     }
 });
